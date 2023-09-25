c28bdbe74ee2b608b146ac32d16e253c83cbc18d
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-25 17:56:10 +0900

    Created app bar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const InstagramCloneApp());
@@ -9,6 +11,55 @@ class InstagramCloneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: Colors.white,
+          secondary: Colors.black,
+        ),
+        useMaterial3: true,
+      ),
+      home: const InstagramCloneHome(),
+    );
+  }
+}
+
+class InstagramCloneHome extends StatelessWidget {
+  const InstagramCloneHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(
+          'Instagram',
+          style: GoogleFonts.lobsterTwo(
+            fontSize: 32,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.favorite_outline,
+              size: 32,
+            ),
+            onPressed: () {
+              print("Tab Favorite");
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              CupertinoIcons.paperplane,
+              size: 32,
+            ),
+            onPressed: () {
+              print("Tab Paperplane");
+            },
+          )
+        ],
+      ),
+      body: const Placeholder(),
+    );
   }
 }
